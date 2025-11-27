3b8d080b2ab6a29fa972b0f0127bf105e27ba751
Ebubeker <rexhaebubeker@gmail.com>
2025-11-27 22:28:59 +0100

    feat

--- src/pages/CookiesPolicy.tsx
+++ src/pages/CookiesPolicy.tsx
@@ -16,7 +16,7 @@ export default function CookiesPolicy() {
         description="מדיניות עוגיות מפורטת של איריס שני יועצת משאבי אנוש - איך אנו משתמשים בעוגיות באתר"
         keywords="מדיניות עוגיות, cookies, עוגיות, איריס שני, משאבי אנוש"
         url="/cookies-policy"
-        image="/og.png"
+        image="/iris-og.png"
       />
       
       <div>
